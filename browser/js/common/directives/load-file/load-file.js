b9635d6f561ebcff91fa9ac66ecaba171378ecf8
TheDooner64 <Robert.Muldoon.1@gmail.com>
2016-01-28 18:58:42 -0500

    User can now upload a .csv file and it is saved to our file-system

--- browser/js/common/directives/load-file/load-file.js
+++ browser/js/common/directives/load-file/load-file.js
@@ -16,29 +16,42 @@ app.controller('LoadFileCtrl', function($scope, Upload, $timeout) {
         $scope.file = file;
         $scope.errFile = errFiles && errFiles[0];
 
-        if (file) {
-            file.upload = Upload.upload({
-                url: '/api/users/upload',
-                method: 'POST',
-                data: {
-                    file: file
-                }
-            });
+        Upload.upload({
+            url: '/api/users/uploads',
+            file: file
+        }).progress(function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+        }).success(function (data, status, headers, config) {
+            console.log("Data: ", data);
+            // console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+        }).error(function (data, status, headers, config) {
+            console.log('error status: ' + status);
+        });
 
-            file.upload.then(function(response) {
+        // if (file) {
+        //     file.upload = Upload.upload({
+        //         url: '/api/users/upload',
+        //         method: 'POST',
+        //         data: {
+        //             file: file
+        //         }
+        //     });
 
-                console.log("Response: ", response);
+        //     file.upload.then(function(response) {
 
-                $timeout(function() {
-                    file.result = response.data;
-                });
-            }, function(response) {
-                if (response.status > 0)
-                    $scope.errorMsg = response.status + ': ' + response.data;
-            }, function(evt) {
-                file.progress = Math.min(100, parseInt(100.0 *
-                    evt.loaded / evt.total));
-            });
-        }
+        //         console.log("Response: ", response);
+
+        //         $timeout(function() {
+        //             file.result = response.data;
+        //         });
+        //     }, function(response) {
+        //         if (response.status > 0)
+        //             $scope.errorMsg = response.status + ': ' + response.data;
+        //     }, function(evt) {
+        //         file.progress = Math.min(100, parseInt(100.0 *
+        //             evt.loaded / evt.total));
+        //     });
+        // }
     }
 });
--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -2,16 +2,18 @@
 var router = require('express').Router();
 module.exports = router;
 var _ = require('lodash');
+var path = require('path');
 
-// Route for file uploads
-// router.post('/:userId/upload', function(req, res, next) {
-//     console.log("User: ", req.params.userId);
-//     console.log("Body: ", req.body);
-//     res.end();
-// });
+var multer = require('multer');
+var uploadFilePath = path.join(__dirname + '/../../../db/upload-files')
 
-router.post('/upload', function(req, res, next) {
-    console.log("Body: ", req.body);
-    res.end();
+// BOBBY NOTE: Need to rename file to user_id and project_id before saving
+    // Add filename property to the multer config object
+var upload = multer({ dest: uploadFilePath });
+
+// Post route
+    // POST /api/users/uploads
+router.post('/uploads', upload.single('file'), function(req, res, next) {
+    var file = req.file;
+    res.send(file);
 });
-
