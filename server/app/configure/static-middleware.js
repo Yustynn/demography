ae7b9b4bbbdd241b850c052d8a5ab29f4b77f00e
John L <john.m.lenehan@gmail.com>
2016-01-28 13:37:20 -0500

    added DC to bower_components

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
     var npmPath = path.join(root, './node_modules');
     var publicPath = path.join(root, './public');
     var browserPath = path.join(root, './browser');
-
+    
     app.use(favicon(app.getValue('faviconPath')));
     app.use(express.static(npmPath));
     app.use(express.static(publicPath));
