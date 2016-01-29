baf8e4f8592ca8de32508eb151c41b77b8ba841c
TheDooner64 <Robert.Muldoon.1@gmail.com>
2016-01-29 17:06:19 -0500

    Rename files to user and dataset ids after saving to filesystem

--- browser/js/profile/profile.js
+++ browser/js/profile/profile.js
@@ -21,17 +21,16 @@ app.controller('ProfileCtrl', function($scope, $state, loggedInUser, ProfileFact
 
     $scope.user = loggedInUser.firstName + " " + loggedInUser.lastName;
 
+    // Funtcion to add the uploaded file to the scope
+    // This is separate from "uploadDataset" so the file can be sent with the metadata on form submission
     $scope.chooseFile = function(file) {
         $scope.file = file;
     }
 
-    $scope.submitProject = function(projectName) {
-        var projectObj = {
-            user: loggedInUser._id,
-            title: projectName
-        };
-
-        ProfileFactory.submitProject($scope.file, projectObj);
+    // Function to send the file and metadata to the factory and then back-end
+    $scope.uploadDataset = function(metaData) {
+        metaData.user = loggedInUser._id;
+        ProfileFactory.uploadDataset($scope.file, metaData);
     }
 
     // $scope.projects;
