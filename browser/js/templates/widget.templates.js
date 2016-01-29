c316f1ec83ae8507e13dda7fe106efd68d0b40ff
jonatans <jonatan@jschumacher.com>
2016-01-29 15:02:42 -0500

    added widget templates

---
+++ browser/js/templates/widget.templates.js
@@ -0,0 +1,11 @@
+app.directive('graphTemplate', function(){
+    return {
+        restrict: 'E',
+        scope: {
+        },
+        templateUrl: 'js/templates/graph.template.html',
+        link: function(scope, element) {
+
+        }
+    };
+});
