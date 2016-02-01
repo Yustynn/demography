07d3be7d80b5617bf61776e8021515bc08aaf5cc
jonatans <jonatan@jschumacher.com>
2016-02-01 17:17:44 -0500

    finished widget factory

--- browser/js/common/factories/widget.factory.js
+++ browser/js/common/factories/widget.factory.js
@@ -4,15 +4,24 @@ app.factory('WidgetFactory', function ($http){
 
         //save new widget upon creation
         create: function(widget) {
-            //$http.post()
+            $http.post('/api/widgets', widget)
+            .then(function(response){
+                return response.data;
+            });
         },
 
         update: function(widget) {
-
+            $http.put('/api/widgets/' + widget._id, widget)
+            .then(function(response) {
+                return response.data;
+            });
         },
 
         delete: function(widget) {
-
+            $http.delete('/api/widgets/' + widget._id)
+            .then(function(response){
+                return response.data;
+            });
         }
     }
 });
