8ead86cae8df34b5ae52e001a43d8d3830934d59
jonatans <jonatan@jschumacher.com>
2016-02-04 16:50:30 -0500

    table graph is now parametric

--- browser/js/common/services/graph.service.js
+++ browser/js/common/services/graph.service.js
@@ -11,14 +11,9 @@
 app.service('GraphService', function() {
     var self = this;
     var ndx, myData, grp;
+
     //Object to store all instances of chart to resize/edit specific chart
     var charts = {};
-
-    this.data = myData;
-
-    // this.getData = function() {
-    //     return myData;
-    // }
 
     this.populateCharts = function(widgetArr) {
         widgetArr.forEach(function(widgetObj) {
@@ -26,6 +21,7 @@ app.service('GraphService', function() {
             if (chartObj) self.create(chartObj.id, chartObj.chartType, chartObj.xAxis, chartObj.yAxis, chartObj.groupType, chartObj.chartOptions)
         })
     }
+
     this.create = function(id, chartType, xAxis, yAxis, groupType, chartOptions) {
         chartOptions = {}; //initialize for now to be empty, users will eventually submit this
         //Gets called after data load, accepts array of chartObjects
@@ -33,9 +29,6 @@ app.service('GraphService', function() {
         var chartWidth = chartContainer.offsetWidth;
         var chartHeight = chartContainer.offsetHeight;
         var chartRadius = chartWidth < chartHeight ? chartWidth / 2 : chartHeight / 2;
-        //console.log(chartWidth, chartHeight, chartRadius);
-        ///var all = ndx.groupAll()
-
 
         var chartObj; //Used for storing all chart options
         var xAxisIsNumber; //Checks if the xAxis is number, and if it needs to be linear or ordinal
@@ -45,19 +38,10 @@ app.service('GraphService', function() {
                 d[xAxis] = Number(d[xAxis]);
                 xAxisIsNumber = true; //Checks if xaxis is ordinal or linear
             };
-            //debugger
-            //console.log('d[xAxis: ',d[xAxis])
             return d[xAxis];
         });
-        var colArr = Object.keys(myData[0]);
 
-        // Object.keys(myData[0]).forEach(function(key){
-        //     colArr.push(function (d){
-        //         return d[key];
-        //     });
-        // })
-
-        console.log(colArr);
+        //TODO: Allow for more group types, including 'none'
         if (groupType === "sum") {
             grp = dim.group().reduceSum(function(d) {
                 if (parseInt(d[yAxis])) d[yAxis] = Number(d[yAxis]);
@@ -71,10 +55,7 @@ app.service('GraphService', function() {
                 return d[yAxis];
             });
         }
-        // var grp = dim.group().reduceSum(function(d) {
-        //     return d.HR;
-        // })
-        //var chart = dc[chartType](chartContainer);
+
         var chart = dc[chartType]('#widget-container-' + id + '> .box-content > .widget-content-container');
 
         //Add chart to Dictionary with a reference to the chart, and it's specific type (pie,bar,etc)
@@ -88,12 +69,10 @@ app.service('GraphService', function() {
             groupType: groupType,
             chartOptions: chartOptions
         };
-
         if (chartType === "pieChart") {
 
             chartObj = makePieChartObject(chartOptions);
             chartObj.radius = chartRadius;
-
         } else if (chartType === "barChart") {
             //margins prevents axes labels from being cutoff
             chartObj = makeBarChartObject(chartOptions, xAxis, yAxis, dim, grp, xAxisIsNumber);
@@ -114,64 +93,19 @@ app.service('GraphService', function() {
                 chartObj.gap = chartHeight * .5 / size;
             }
         } else if (chartType === "dataTable") {
+            chartObj = makeTableChartObject(chartOptions)
 
-            chart
-                .width(400)
-                .height(400)
-                .dimension(dim)
-                .group(function(d) {
-                    return d[xAxis] //create a new header for grouped values
-                })
-                .size(1000)
-                .columns(colArr)
-                .sortBy(function(d) {
-                    return d[yAxis];
-                })
-                .order(d3.descending)   //can be ascending and descending
-
-                //ADD CUSTOM CLASS TO LABEL ROWS
-                // .on('renderlet', function(table) {
-                //     table.selectAll('#widget-container-' + id + '> .box-content > .widget-content-container').classed('info', true);
-                // });
-
-            dc.renderAll();
-
-
-            // chartObj = makeTableChartObject(chartOptions)
         } else if (chartType === "dataCount") {
-            chartObj = makeDataCountChartObject(chartOptions)
+            chartObj = makeDataCountChartObject(chartOptions, xAxis, yAxis)
+        }
 
-
-
-        };
-
-
-
-        // chartObj.width = 200;
-        // chartObj.height = 200;
-        // console.log('height', chartHeight);
-        // if (chartType === 'dataCount' || chartType === "dataTable") {
-        //     chartObj.group = function(d) {
-        //         return "TABLE"
-        //     };
-        //     chartObj.dimension = ndx.dimension(function(d) {
-        //         return d.education;
-        //     }).group();
-        //     if (chartType === 'dataCount') {
-        //         chartObj.html = {
-        //             some: '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records' +
-        //                 ' | <a href=\'javascript:dc.filterAll(); dc.renderAll();\'\'>Reset All</a>',
-        //             all: 'All <strong>%total-count</strong> records selected.'
-        //         }
-        //     }
-        // } else {
-        //     chartObj.dimension = dim;
-        //     chartObj.group = grp;
-        // }
-        //passing in graphCount(i.e. the id) to keep functions expected input the same for both graph
-        //creation and graph editing
-        // createChart(id, chartObj)
-        // return charts['chart' + id];
+        //set default options here if they dont already exist:
+        if(!chartObj.width) chartObj.width = chartWidth;
+        if(!chartObj.height) chartObj.width = chartHeight;
+        if(!chartObj.dimension) chartObj.dimension = dim;
+        if(!chartObj.group) chartObj.group = grp;
+        createChart(id, chartObj)
+        return charts['chart' + id];
     };
 
     this.resize = function(id) {
@@ -213,7 +147,6 @@ app.service('GraphService', function() {
 
             dc.renderAll();
         }, 500)
-
     }
 
     //load data
@@ -221,7 +154,6 @@ app.service('GraphService', function() {
         myData = dataSet;
         ndx = crossfilter(dataSet);
     }
-
 
     //Function that takes chartId and renders it with all options
     function createChart(id, chartOptions) {
@@ -232,7 +164,7 @@ app.service('GraphService', function() {
             //console.log(key, ":", chartOptions[key])
             chart[key](chartOptions[key])
         });
-        debugger;
+
         dc.renderAll();
     };
     //Pie Chart Option creator
@@ -366,45 +298,44 @@ app.service('GraphService', function() {
     };
 
     //Data Table Chart Option creator-has superfluous parameters for testing
-    function makeTableChartObject(chartOptions, numRows) {
+    function makeTableChartObject(chartOptions,x,y,numRows) {
         var tableChartOptions = {
-            //ATTEMPT 1:
-            //  chartObj.columns = [
-            //      function(d) {
-            //          debugger;
-            //          return d[0]; },
-            //      function(d) { return d[1]; },
-            //      function(d) { return d[2]; },
-            //      function(d) { return d[3]; },
-            //      function(d) { return d[4]; }
-            // ];
-            //ATTEMPT 2:
-            columns: [function(d) {
-                console.log(d)
-                return d.key;
-            }, function(d) {
-                return d.key;
-            }],
-            //ATTEMPT 3: HARDCODING WORKS
-            //columns: ['Age', 'workclass', 'fnlwgt', 'Education', 'martial-status'],
-            order: d3.descending,
-            size: 10
+
+            //ADD CUSTOM CLASS TO LABEL ROWS
+            //.on('renderlet', function(table) {
+            //     table.selectAll('#widget-container-' + id + '> .box-content > .widget-content-container').classed('info', true);
+            // });
+            sortBy: function(d) {
+                return d[y];
+            },
+            columns: Object.keys(myData[0]),    //TODO: define colArr here, not above.
+            group: function(d) {
+                return d[x] //create a new header for grouped values
+            },
+            order: d3.descending, //can be ascending and descending
+            size: numRows ? numRows : 10    //how many rows to display - make this dynamic based on widget height
         };
 
         Object.keys(chartOptions).forEach(function(key) {
             tableChartOptions[key] = chartOptions[key];
         });
-        debugger;
         return tableChartOptions;
     };
 
     function makeDataCountChartObject(chartOptions) {
-        var dataCountOptions = {};
+        var dataCountOptions = {
+            group : ndx.groupAll(),
+            dimension: ndx,
+            html : {
+                some: '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records' +
+                    ' | <a href=\'javascript:dc.filterAll(); dc.renderAll();\'\'>Reset All</a>',
+                all: 'All <strong>%total-count</strong> records selected.'
+            }
+        };
 
         Object.keys(chartOptions).forEach(function(key) {
             dataCountOptions[key] = chartOptions[key];
         });
-
         return dataCountOptions;
     }
 })
