ce18b984a9c5d9b3ed3ba9b590faa8c8e8bcfe76
Honan <jrp12@hampshire.edu>
2016-02-05 11:38:07 -0500

    1

--- browser/js/filters/objectToArray.filter.js
+++
@@ -1,9 +0,0 @@
-app.filter('object2Array', function() {
-    return function(input) {
-        var out = [];
-        for (i in input) {
-            out.push(input[i]);
-        }
-        return out;
-    }
-});
