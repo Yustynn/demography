10179dc946edff47ebda2fff6d306f1024645653
TheDooner64 <Robert.Muldoon.1@gmail.com>
2016-02-09 20:49:39 -0500

    Add ability to cancel out of modal and delete blank widget

--- browser/js/widget/widget.settings.controller.js
+++ browser/js/widget/widget.settings.controller.js
@@ -1,12 +1,14 @@
-app.controller('WidgetSettingsCtrl', function ($scope, $timeout, $rootScope, $uibModalInstance, widget, WidgetFactory, GraphService, ChartService, dataset, element, graphSize) {
+app.controller('WidgetSettingsCtrl', function($scope, $timeout, $rootScope, $uibModalInstance, $uibModalStack, widget, WidgetFactory, GraphService, ChartService, dataset, element, graphSize) {
     $scope.widget = widget;
     $scope.chartType;
 
     $scope.axisDropdowns = {
-        objectKeys : Object.keys(dataset.jsonData[0])
-        .map(function(key){
-            return {key: key};
-        })
+        objectKeys: Object.keys(dataset.jsonData[0])
+            .map(function(key) {
+                return {
+                    key: key
+                };
+            })
     };
 
     // Collapse all "settings" accordions by default in the add chart modal
@@ -15,13 +17,12 @@ app.controller('WidgetSettingsCtrl', function ($scope, $timeout, $rootScope, $ui
     $scope.lineChartCollapsed = true;
 
     // Functions to toggle accordions
-        // Consider creating a single, dynamic function that can do all of this
+    // Consider creating a single, dynamic function that can do all of this
     $scope.toggleBarChartAccordion = function() {
         if ($scope.barChartCollapsed) {
             $scope.barChartCollapsed = false;
             $scope.chartType = "barChart";
-        }
-        else {
+        } else {
             $scope.barChartCollapsed = true;
             $scope.chartType = undefined;
         }
@@ -38,14 +39,14 @@ app.controller('WidgetSettingsCtrl', function ($scope, $timeout, $rootScope, $ui
     }
 
     //This will be an map with different themes, for now a single array
-    $scope.colorOptions = ['#3182bd','#6baed6','#9ecae1', '#c6dbef','#e6550d','#fd8d3c','#fdae6b','#fdd0a2','#31a354','#74c476','#a1d99b','#c7e9c0','#756bb1','#9e9ac8','#bcbddc','#dadaeb','#636363','#969696','#bdbdbd','#d9d9d9']
-    //$scope.groupOptions = [{val:'sum'}, {val:'count'}];
+    $scope.colorOptions = ['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d', '#fd8d3c', '#fdae6b', '#fdd0a2', '#31a354', '#74c476', '#a1d99b', '#c7e9c0', '#756bb1', '#9e9ac8', '#bcbddc', '#dadaeb', '#636363', '#969696', '#bdbdbd', '#d9d9d9']
+        //$scope.groupOptions = [{val:'sum'}, {val:'count'}];
 
     var selectedColumns = [];
     angular.copy($scope.axisDropdowns.objectKeys, selectedColumns);
 
     var selection = {
-        group:'count'
+        group: 'count'
     }
 
     $scope.graphGroups = {
@@ -53,45 +54,41 @@ app.controller('WidgetSettingsCtrl', function ($scope, $timeout, $rootScope, $ui
     }
 
     $scope.form = {
-        title: widget.title,    //update title
-        labelX: widget.labelX,  //update data on X
-        labelY: widget.labelY,   //update data on Y
+        title: widget.title, //update title
+        labelX: widget.labelX, //update data on X
+        labelY: widget.labelY, //update data on Y
         group: selection.group,
         graphGroup: 'Group1',
         orderBy: 'ascending',
         columns: selectedColumns
     };
 
-    // BOBBY NOTE: Moved this to the directive
-    // $scope.addGraphGroup = function() {
-    //     var newGroup = 'Group'+ ($scope.graphGroups.options.length+1);
-    //     WidgetFactory.addGraphGroup(newGroup);
-    //     $scope.form.graphGroup= newGroup;
-    //     $scope.graphGroups.options = WidgetFactory.getGraphGroups();
-    // };
-
-    $scope.dismiss = function() {
-        $uibModalInstance.dismiss();
+    $scope.addGraphGroup = function() {
+        var newGroup = 'Group'+ ($scope.graphGroups.options.length+1);
+        WidgetFactory.addGraphGroup(newGroup);
+        $scope.form.graphGroup= newGroup;
+        $scope.graphGroups.options = WidgetFactory.getGraphGroups();
     };
 
     $scope.remove = function() {
-        console.log("$scope.dashboard: ", $scope.dashboard);
+        $uibModalStack.dismissAll();
+        WidgetFactory.delete(widget._id);
         $scope.dashboard.widgets.splice($scope.dashboard.widgets.indexOf(widget), 1);
-        $uibModalInstance.close();
     };
 
     $scope.submit = function() {
         angular.extend(widget, $scope.form); //update widget with settings from form
         $uibModalInstance.close(widget);
+        $uibModalStack.dismissAll();
 
         var _chartOptions = {
             order: $scope.form.orderBy ? d3[$scope.form.orderBy] : d3.ascending,
-            columns: $scope.form.columns.length > 0 ? $scope.form.columns.map(function(col){return col.key; }) : null
+            columns: $scope.form.columns.length > 0 ? $scope.form.columns.map(function(col) {
+                return col.key;
+            }) : null
         };
 
-        // this modal is used to both create and update graphs. hence this logic:
-            // BOBBY NOTE: This logic needs to change
-        if($scope.chartType) {
+        if ($scope.chartType) {
 
             // Creates new graph
             var chartConfig = {
@@ -110,13 +107,22 @@ app.controller('WidgetSettingsCtrl', function ($scope, $timeout, $rootScope, $ui
             };
 
             widget.chartObject = ChartService.create(chartConfig);
-        }
-        else {
+        } else {
             //only get updated properties:
-           console.error("UPDATING NOT YET WORKING");
+            console.error("UPDATING NOT YET WORKING");
             //widget.chartObject = ChartService.update(chartConfig);
         }
         WidgetFactory.update(widget);
     };
+
+    // This is used to close the model with the escape key
+        // Removing the widget from this failure callback currently doesn't work
+    $uibModalInstance.result.then(function() {
+        console.log("Form successfully submitted!");
+        $uibModalStack.dismissAll();
+    }, function() {
+        console.log("Failure handler ran!");
+        $uibModalStack.dismissAll();
+    });
 
 });
