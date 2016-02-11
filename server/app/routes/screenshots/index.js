9ef6292e19c4beb6f31b0e173538abdfc98fc95a
Tom Sharon <tomsharo9591@gmail.com>
2016-02-11 11:25:21 -0500

    cropping: true

--- server/app/routes/screenshots/index.js
+++ server/app/routes/screenshots/index.js
@@ -8,7 +8,6 @@ module.exports = router;
 
 // /api/screenshots
 router.post("/", function(req, res, next) {
-
 	var cookieString = `connect.sid=${req.cookies['connect.sid']}`;
     var pageres = new Pageres({cookies: [cookieString], filename: req.body.dashboardId, selector: '#main > div > div.ng-scope.gridster.gridster-desktop.gridster-loaded'})
         .src('http://localhost:1337/users/' + req.user._id + '/datasets/' + req.body.datasetId + '/dashboards/' + req.body.dashboardId, ['1024x768'], {crop: true})
