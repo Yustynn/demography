6065c17ed9416d4a973770842ca91be8a31b4c29
jonatans <jonatan@jschumacher.com>
2016-02-16 12:43:05 -0500

    secret

--- server/env/development.js
+++ server/env/development.js
@@ -1,7 +1,8 @@
-process.env.TOKEN_SECRET = "qE19G2Lnmf8EXE60gdztw8ajMHfTc0S9"; // BOBBY NOTE: This looks like a dupe with the token listed below
+var authConfig = require('../../secrets.js');
+//process.env.TOKEN_SECRET = "qE19G2Lnmf8EXE60gdztw8ajMHfTc0S9"; // BOBBY NOTE: This looks like a dupe with the token listed below
 module.exports = {
   // "DATABASE_URI": "mongodb://localhost:27017/demography",
-  "DATABASE_URI": "mongodb://BobbyM:dashjspassword@ds061405.mongolab.com:61405/heroku_8krs0wlv",
+  "DATABASE_URI": authConfig.DATABASE_URI,
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
@@ -18,13 +19,13 @@ module.exports = {
     "clientSecret": "rIwPdYD8FBtQu0aEsprOb9Bl",
     "callbackURL": "http://localhost:1337/auth/google/callback"
   },
-  "TOKEN_SECRET": "qE19G2Lnmf8EXE60gdztw8ajMHfTc0S9", //http://randomkeygen.com/
-  "PHANTOM_SECRET": "56b79c90350318795f366482",
-  "PHANTOM_API": "eyJhbGciOiJIUzI1NiJ9.NTZiNzljOTAzNTAzMTg3OTVmMzY2NDgy.JpvhWq8fKnFaUzp8KJoQ3YEvWMI8-h0_Un7LnYyuBio",
+  "TOKEN_SECRET": authConfig.TOKEN_SECRET,
+  "PHANTOM_SECRET": authConfig.PHANTOM_SECRET,
+  "PHANTOM_API": authConfig.PHANTOM_API,
   "SCREENSHOT_URL" : 'http://localhost:1337/users/',
   "S3": {
-    "ACCESS_KEY_ID":"AKIAI6V5AMRSDNVYREGA",
-    "SECRET_ACCESS_KEY":"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",
-    "SCREENSHOT_URL" : 'https://s3.amazonaws.com/dashjs/'
+    "ACCESS_KEY_ID":authConfig.S3.ACCESS_KEY_ID,
+    "SECRET_ACCESS_KEY":authConfig.S3.SECRET_ACCESS_KEY,
+    "SCREENSHOT_URL":authConfig.S3.SCREENSHOT_URL
   }
 };
